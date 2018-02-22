907ec2c01c96e657ad908f8a7e68c43c0a471c5c
lyan <tntlagf93@mail.ru>
2018-02-22 17:11:43 +0300

    minor

--- utils.py
+++ utils.py
@@ -4,17 +4,15 @@ from PIL import Image
 
 def get_transform_by(mode, crop_size, image_size):
     if mode == 'train':
-        transform = transforms.Compose([
+        return transforms.Compose([
             transforms.CenterCrop(crop_size),
             transforms.Resize(image_size, interpolation=Image.ANTIALIAS),
             transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
             transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
     else:
-        transform = transforms.Compose([
+        return transforms.Compose([
             transforms.CenterCrop(crop_size),
             transforms.Scale(image_size, interpolation=Image.ANTIALIAS),
             transforms.ToTensor(),
             transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-
-    return transform
